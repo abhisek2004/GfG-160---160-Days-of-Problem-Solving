9683483832712e3c438465e9fcbc69fa052644b5
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-11-16 21:02:05 +0530

    1 answer

---
+++ Code/DAY 1/1.py
@@ -0,0 +1,22 @@
+# User function Template for python3
+class Solution:
+    def getSecondLargest(self, arr):
+        x = list(set(arr))
+        if len(x) > 1:
+            x.sort()
+            return x[-2]
+        else:
+            return -1
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.getSecondLargest(arr)
+        print(ans)
+        print("~")
+# } Driver Code Ends
