bfe81ce945dcace8ab82f2e8d8ffa85d42f319bf
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-11-18 00:57:46 +0530

    day 3 added

---
+++ Code/DAY 3/1.py
@@ -0,0 +1,32 @@
+import math
+
+
+class Solution:
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        d = d % n
+        arr[:] = arr[d:] + arr[:d]
+
+
+def main():
+    T = int(input())
+
+    while (T > 0):
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ob.rotateArr(A, D)
+
+        for i in A:
+            print(i, end=" ")
+
+        print()
+
+        T -= 1
+
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
