9b4b71604762aba85bcf79450e73c8ae9c851115
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-12-12 08:09:21 +0530

    4 SEARCHING Day 27 answer

---
+++ Code/DATA STRUCTURE AND ALGORITHMS/4 SEARCHING/DAY 27/1.py
@@ -0,0 +1,20 @@
+import bisect
+
+
+class Solution:
+    def countFreq(self, arr, target):
+        # code here
+        return arr.count(target)
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while t:
+        t -= 1
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ans = ob.countFreq(A, D)
+        print(ans)
+        print("~")
