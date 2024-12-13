a3ccc5abe7515f728b626b818267493bd368be03
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-12-13 07:44:54 +0530

    Day 28 answer

---
+++ Code/DATA STRUCTURE AND ALGORITHMS/4 SEARCHING/DAY 28/1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, arr):
+        # complete the function here
+        data = min(arr)
+        return data
+
+
+def main():
+    T = int(input())
+
+    while T > 0:
+        a = list(map(
+            int,
+            input().strip().split()))  # Convert input to list of integers
+        print(Solution().findMin(a))  # Call findMin with the array 'a'
+        T -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
