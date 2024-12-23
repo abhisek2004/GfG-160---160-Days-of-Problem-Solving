2f484270c1455345b1f84d53f511267a48b03161
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2024-12-23 23:43:15 +0530

    Day 38 Answer

---
+++ Code/DATA STRUCTURE AND ALGORITHMS/5 MATRIX/DAY 38/1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def searchRowMatrix(self, mat, x):
+        a = len(mat)
+        for i in range(a):
+            for j in range(len(mat[i])):
+                if x == mat[i][j]:
+                    return True
+        return False
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        r = int(data[index])
+        c = int(data[index + 1])
+        index += 2
+        matrix = []
+        for i in range(r):
+            row = list(map(int, data[index:index + c]))
+            matrix.append(row)
+            index += c
+        x = int(data[index])
+        index += 1
+        ob = Solution()
+        if ob.searchRowMatrix(matrix, x):
+            print("true")
+        else:
+            print("false")
+        print("~")
