5c0990a106fbb8ad148f6995a444d677eeee9ba5
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-01-23 19:27:54 +0530

    BONUS PROBLEMS 8  Prefix Sum Answer 2

---
+++ Code/BONUS PROBLEMS/8  Prefix Sum/2.py
@@ -0,0 +1,26 @@
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+# } Driver Code Ends
+# User function Template for python3
+class Solution:
+    def longestSubarray(self, arr, k):
+        # Code Here
+
+        # {
+     # Driver Code Starts.
+
+
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+
+        arr = [int(x) for x in input().strip().split()]
+        k = int(input())
+
+        ob = Solution()
+        print(ob.longestSubarray(arr, k))
+        print("~")
+        t -= 1
+# } Driver Code Ends
