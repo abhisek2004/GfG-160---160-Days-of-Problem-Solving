ae0dc91607b94b824f36f9b08968d8feedda747d
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-01-23 19:33:29 +0530

    BONUS PROBLEMS 8  Prefix Sum Answer 5

---
+++ Code/BONUS PROBLEMS/8  Prefix Sum/5.py
@@ -0,0 +1,25 @@
+# User function Template for python3
+import bisect
+
+
+class Solution:
+    def longestSubarrayDivK(self, arr, k):
+        # Complete the function
+
+
+        # {
+     # Driver Code Starts
+        # Initial Template for Python 3
+        # Main
+if __name__ == '__main__':
+    t = int(input())
+    while t:
+        t -= 1
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ans = ob.longestSubarrayDivK(A, D)
+        print(ans)
+        print("~")
+# } Driver Code Ends
