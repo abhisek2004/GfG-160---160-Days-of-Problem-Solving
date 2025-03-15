85271acfdcf8c4ea7a2021ea0273c973f1c18faa
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-03-15 23:30:54 +0530

    Day 120 Answer

--- Code/DATA STRUCTURE AND ALGORITHMS/15 Dynamic Programming/DAY 120/1.py
+++ Code/DATA STRUCTURE AND ALGORITHMS/15 Dynamic Programming/DAY 120/1.py
@@ -1,26 +1,29 @@
-Coin Change(Minimum Coins)
-Difficulty: MediumAccuracy: 26.74 % Submissions: 245K+Points: 4
-You are given an array coins[], where each element represents a coin of a different denomination, and a target value sum. You have an unlimited supply of each coin type {coins1, coins2, ..., coinsm}.
+class Solution:
+    def minCoins(self, coins, sum):
+        i = float('inf')
+        dp = [i]*(sum+1)
+        dp[0] = 0
 
-Your task is to determine the minimum number of coins needed to obtain the target sum. If it is not possible to form the sum using the given coins, return -1.
+        for c in coins:
+            for j in range(c, sum+1):
+                dp[j] = min(dp[j], 1+dp[j-c])
+        return dp[sum] if dp[sum] != i else -1
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+# Position this line where user code will be pasted.
+# Initial Template for Python 3
 
-Examples:
 
-Input: coins[] = [25, 10, 5], sum = 30
-Output: 2
-Explanation: Minimum 2 coins needed, 25 and 5
-Input: coins[] = [9, 6, 5, 1], sum = 19
-Output: 3
-Explanation: 19 = 9 + 9 + 1
-Input: coins[] = [5, 1], sum = 0
-Output: 0
-Explanation: For 0 sum, we do not need a coin
-Input: coins[] = [4, 6, 2], sum = 5
-Output: -1
-Explanation: Not possible to make the given sum.
+if __name__ == "__main__":
+    t = int(input())
+    while t > 0:
+        k = int(input())
+        arr = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.minCoins(arr, k)
+        print(res)
+        print("~")
+        t -= 1
 
-Constraints:
-1 ≤ sum * coins.size() ≤ 106
-0 <= sum <= 104
-1 <= coins[i] <= 104
-1 <= coins.size() <= 103
+# } Driver Code Ends
